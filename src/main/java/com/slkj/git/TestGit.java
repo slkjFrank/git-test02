e0aa325f655bba35e7366d2fcab0d30586199493
Frank <Frank@qq.com>
2022-07-03 11:11:56 +0800

    first commit

---
+++ src/main/java/com/slkj/git/TestGit.java
@@ -0,0 +1,17 @@
+package com.slkj.git;
+
+/**
+ * @author Frank
+ * @version 1.0
+ * @date 2022/7/3 11:04
+ * @description:
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+    }
+}
