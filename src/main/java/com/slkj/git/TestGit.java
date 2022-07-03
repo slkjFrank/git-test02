9daed46b301da6c8f0e0ab347b20610c38341748
slkjFrank <108522833+slkjFrank@users.noreply.github.com>
2022-07-03 13:01:09 +0800

    Update TestGit.java

--- src/main/java/com/slkj/git/TestGit.java
+++ src/main/java/com/slkj/git/TestGit.java
@@ -19,5 +19,6 @@ public class TestGit {
         System.out.println("hello git! forth commit!->Merge branch!->new branch!");
         System.out.println("hello git! new branch!");
         System.out.println("hello git! test push remote");
+        System.out.println("hello git! test pull remote");
     }
 }
